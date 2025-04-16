d4b183ccc126613a9507a2fe9713fbe663ce0a92
cudy789 <knuts983@umn.edu>
2025-04-16 01:18:41 -0500

    Added updater functionality. Most automations should work.

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -7,16 +7,33 @@ else
     RELEASE_VERSION="MAPLE-R25-0-1"
 fi
 
+IMAGE_NAME="maple"
+IMAGE_TAG="latest"
+
 if [ "$ARM" = "1" ] || [ "$( uname -m )" = "aarch64" ]; then
   ARCH="arm64"
+  ARCH_D="--platform=linux/arm64"
 else
   ARCH="x86"
+  ARCH_D="--platform=linux/amd64"
+  IMAGE_TAG="X64"
 fi
 mkdir -p release/"$ARCH"
 
 # Build MAPLE
 rm -rf build
-bash scripts/run-common.sh "mkdir -p build && cd build && cmake .. && make -j4"
+
+docker run --rm -h maple --name maple --group-add sudo --group-add video --add-host $IMAGE_NAME-$HOSTNAME:127.0.0.1 --network host \
+  --user=$(id -u $USER):$(id -g $USER) \
+  --volume="/etc/passwd:/etc/passwd:ro" \
+  --volume="/etc/shadow:/etc/shadow:ro" \
+  --volume="$HOME:$HOME" \
+  --workdir="$(pwd)" \
+  --privileged \
+  --pids-limit=-1 \
+  $ARCH_D \
+  rogueraptor7/$IMAGE_NAME:$IMAGE_TAG /bin/bash -c "mkdir -p build && cd build && cmake .. && make -j4"
+
 # Make sure there aren't any logfiles
 rm -rf build/logs/*
 
